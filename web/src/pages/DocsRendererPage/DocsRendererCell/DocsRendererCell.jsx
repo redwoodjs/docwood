ba3b4e5da387af50bd09e44b71175c083087fd52
Rob Cameron <cannikin@fastmail.com>
2024-04-04 16:26:04 -0700

    Update mdx component

--- web/src/pages/DocsRendererPage/DocsRendererCell/DocsRendererCell.jsx
+++ web/src/pages/DocsRendererPage/DocsRendererCell/DocsRendererCell.jsx
@@ -15,32 +15,25 @@ import { DOCS_ROOT_PATH } from 'src/lib/paths'
 import CustomIndexComponent from './CustomIndexComponent'
 
 export const data = async ({ docPath }) => {
-  // if docPath is undefined, assume the root `index` of the whole /docs dir
+  let md, MdxComponent, IndexComponent
+
   // first try looking for an md file with the exact name of the URL path
+  // if docPath is undefined, assume the root `index` of the whole /docs dir
+  // and just look for `index.md`
   let filePath = path.join(DOCS_ROOT_PATH, `${docPath || 'index'}.md*`)
 
-  // if not found, look for an index.md file in the directory of docPath
   if (!fg.sync(filePath).length) {
+    // if not found, look for an index.md file in the directory of docPath
     filePath = path.join(DOCS_ROOT_PATH, docPath, `index.md*`)
   }
 
-  // look for the file that matches the glob
+  // see if fast-glob finds any files that match the path
   const matchingFilePath = fg.sync(filePath)[0]
 
-  console.info('\n\n***************************\n\n')
-  console.info('docPath: ', docPath)
-  console.info('filePath: ', filePath)
-  console.info('matchingFilePath: ', matchingFilePath)
-  console.info('\n\n***************************\n\n')
-
-  let md, MdxComponent, IndexComponent
-
   if (!matchingFilePath) {
-    console.info('Custom index')
     // no file and no index, so create one
     IndexComponent = CustomIndexComponent(docPath)
   } else if (matchingFilePath.match(/\.mdx$/)) {
-    console.info('MDX file')
     // mdx file
     MdxComponent = (
       await evaluate(fs.readFileSync(matchingFilePath, 'utf8'), {
@@ -50,12 +43,9 @@ export const data = async ({ docPath }) => {
       })
     ).default
   } else {
-    console.info('Plain md file')
     // plain md file
     md = fs.readFileSync(matchingFilePath, 'utf8')
   }
-
-  console.info(MdxComponent)
 
   return { ...fm(md), MdxComponent, IndexComponent }
 }
