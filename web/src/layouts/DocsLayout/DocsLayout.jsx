98639c44545e4a9f99bbfe4042487ab84d75f950
Rob Cameron <cannikin@fastmail.com>
2024-04-03 21:03:00 -0700

    Little margin around the page

--- web/src/layouts/DocsLayout/DocsLayout.jsx
+++ web/src/layouts/DocsLayout/DocsLayout.jsx
@@ -1,6 +1,6 @@
 const DocsLayout = ({ children }) => {
   return (
-    <div className="mt-8 border-2 border-dashed border-gray-600 p-4">
+    <div className="mt-8 border-2 border-dashed border-gray-600 m-4 p-4">
       <h1 className="-ml-4 -mt-10 text-gray-600 font-semibold">DocsLayout</h1>
       <div className="max-w-screen-lg mx-auto">
         <div className="">{children}</div>
