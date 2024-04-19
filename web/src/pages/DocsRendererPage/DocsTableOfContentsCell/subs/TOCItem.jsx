028992a94ec3aaab1a6165bf0515a601b0e77e5b
Rob Cameron <cannikin@fastmail.com>
2024-04-19 13:46:41 -0700

    Remove special characters in ToC anchor links

--- web/src/pages/DocsRendererPage/DocsTableOfContentsCell/subs/TOCItem.jsx
+++ web/src/pages/DocsRendererPage/DocsTableOfContentsCell/subs/TOCItem.jsx
@@ -1,12 +1,15 @@
 'use client'
 
 export const TOCItem = ({ node }) => {
-  const id = node.value.toLowerCase().replaceAll(' ', '-').replaceAll('.', '')
+  const id = node.value
+    .toLowerCase()
+    .replace(/[^\w-]/g, '-') // replace non-word characters with a dash
+    .replace(/-+/g, '-') // replace multiple dashes with a single one
+    .replace(/-+$/, '') // remove any dashes at the end of the string
   return (
     <li className="my-1 leading-4">
       <a href={`#${id}`}>{node.value}</a>
       {node.children.length > 0 && (
-        <ul>
         <ul className="list-none border-l border-gray-300 pl-2">
           {node.children.map((child) => (
             <TOCItem key={child.value + child.depth} node={child} />
